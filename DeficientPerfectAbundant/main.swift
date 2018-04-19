b897bfe27e054c2b806e21fd541bd2e252f362a5
Diaz Octavio <odiaz@lcs-mba3082.local>
2018-04-19 11:06:17 -0400

    problem solved B )

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -8,15 +8,52 @@
 
 import Foundation
 
-// Get the user input
-var rawInput = readLine()
+//let n : Int = readLine()
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+// INPUT
+var n = 0
+while 1 == 1 {
+    
+    // Test #1: Wait for input and make sure it is not nil
+    guard let givenInput = readLine() else {
+        // We got nil input, so prompt again
+        continue
+    }
+    
+    // Test #2: Can we make the string
+    guard let givenInteger = Int(givenInput) else {
+        // If we got here, the input couldn't be made into an integer
+        // For example, "five"
+        continue
+    }
+    
+    // Test #3: Is the integer in the correct range?
+    if givenInteger < 1 || givenInteger > 32500 {
+        // Integer is too low or too high
+        continue
+    }
+    
+    // If we got here, we know the input is what we want
+    n = givenInteger
+    break // MUST be there to exit the infinite while loop
+    
+}
 
-for i ... n/2 {
+// PROCESS
+var divisorSum = 0
+for i in 1...n/2 {
     if n % i == 0 {
-        
+        divisorSum += n
     }
 }
+// OUTPUT
+if n == divisorSum {
+    print("/(n) is a perfect number")
+}
+if n < divisorSum {
+    print("/(n) is a defficient number")
+}
+if n > divisorSum {
+    print("/(n) is a abundant number")
+}
+
