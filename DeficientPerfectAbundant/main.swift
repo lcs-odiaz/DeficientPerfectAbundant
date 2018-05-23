30d955abd036c18f57dc9f67559afabf5f924dca
Diaz Octavio <odiaz@lcs-mba3082.lcs.on.ca>
2018-05-22 21:31:39 -0400

    learning goal 1

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -13,7 +13,7 @@ import Foundation
 // INPUT
 var n = 0
 while 1 == 1 {
-    
+    print("What is your number?")
     // Test #1: Wait for input and make sure it is not nil
     guard let givenInput = readLine() else {
         // We got nil input, so prompt again
@@ -21,14 +21,14 @@ while 1 == 1 {
     }
     
     // Test #2: Can we make the string
-    guard let givenIntger = Int(givenInput) else {
+    guard let givenInteger = Int(givenInput) else {
         // If we got here, the input couldn't be made into an integer
         // For example, "five"
         continue
     }
     
     // Test #3: Is the integer in the correct range?
-    if givenInteger < 1 || givenInteger > 32500 {
+    if givenInteger < 1 || givenInteger >= 32500 {
         // Integer is too low or too high
         continue
     }
@@ -41,19 +41,22 @@ while 1 == 1 {
 
 // PROCESS
 var divisorSum = 0
-for i in 1...n/2 {
+for i in 1...n/2{
     if n % i == 0 {
         divisorSum += n
+        //divisor sum must be a variable because everytime the function loops
+        //n is added to the divisorSum 
     }
 }
+
 // OUTPUT
 if n == divisorSum {
-    print("/(n) is a perfect number")
-}
-if n < divisorSum {
-    print("/(n) is a defficient number")
+    print("\(n) is a perfect number")
 }
 if n > divisorSum {
-    print("/(n) is a abundant number")
-}//blah blahh
+    print("\(n) is a defficient number")
+}
+if n < divisorSum {
+    print("\(n) is a abundant number")
+}
 
